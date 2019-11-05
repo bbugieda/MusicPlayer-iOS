d5baaa9246ae4d9a007c0aa271d41337b214d9fb
bbugieda <brian.bugieda@gmail.com>
2019-11-05 01:35:28 -0500

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Brian Bugieda on 11/5/19.
+//  Copyright © 2019 Brian Bugieda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
